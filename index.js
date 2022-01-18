ff0a776d9fa8b058e7298486ac33c677ad8ea5fb
Blake Hakkila <blake.austin.hakkila@gmail.com>
2022-01-17 18:49:10 -0800

    GraphQL Hello World

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const express = require('express')
+var { graphqlHTTP } = require('express-graphql');
+var { buildSchema } = require('graphql');
+
 const app = express()
 const port = 3000
 
@@ -9,3 +12,27 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
+
+
+// Construct a schema, using GraphQL schema language
+var schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+// The root provides a resolver function for each API endpoint
+var root = {
+  hello: () => {
+    return 'Hello world!';
+  },
+};
+
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
+
+app.listen(4000);
+console.log('Running a GraphQL API server at http://localhost:4000/graphql');
