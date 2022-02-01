88db6512c0f66acf0735fee14e089c52de784d9c
Jonathan Alexander <me@jmalexan.com>
2022-01-31 19:13:35 -0800

    Basic DB functions structure

---
+++ db-wrapper.js
@@ -0,0 +1,39 @@
+const knex = require('knex');
+
+class MyDatabase {
+    constructor(knexConfig) {
+        this.knex = knex(knexConfig);
+    }
+
+//   getFruits() {
+//     this.knex
+//       .select("*")
+//       .from("fruit")
+//       .where({ id: 1 })
+//       .then((ret) => {
+//           console.log(ret);
+//       })
+//   }
+
+  getLastTransaction() {
+
+  }
+
+  getTransactionBetween(initDate, endDate) {
+
+  }
+
+  addTransaction(transaction) {
+
+  }
+
+  addItem(item) {
+
+  }
+
+  getItemWithName(name) {
+
+  }
+}
+
+module.exports = MyDatabase;
--- index.js
+++ index.js
@@ -1,3 +1,30 @@
+const dbwrapper = require("./db-wrapper.js");
+
+const knexConfig = {
+  client: "mysql",
+  connection: {
+    host : '127.0.0.1',
+    port : 3306,
+    user : 'your_database_user',
+    password : 'your_database_password',
+    database : 'myapp_test'
+  }
+}
+
+const db = new dbwrapper(knexConfig);
+
+
+
+
+
+
+
+
+
+
+
+
+
 const express = require('express')
 const app = express()
 const port = 3000
